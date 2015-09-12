84d697d8868e39bfbe087d55d877ba3e824af986
SohamJ <soham4jam@gmail.com>
2015-09-12 19:56:10 -0400

    update kernel toolchain to uber 5.2
    Change-Id: I7c6e02b111dfe93c73ec060a50b8dd122a4ad034

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := falcon_defconfig
 ENABLE_GRAPHITE := true
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.2/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Rom Toolchain
