947d9b79d748c8603b618e62cf4fd40a2ad013b6
srisurya95 <srisurya95@gmail.com>
2015-11-08 18:45:59 +0100

    Orion-ify
    Change-Id: I4eb2c908d41c6413c7e83b5456f32a60bdaead07

--- orion.mk
+++ orion.mk
@@ -1,10 +1,10 @@
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common Orion stuff.
+$(call inherit-product, vendor/orion/config/common_full_phone.mk)
 
 # Inherit device specific
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := slim_falcon
+PRODUCT_NAME := orion_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
